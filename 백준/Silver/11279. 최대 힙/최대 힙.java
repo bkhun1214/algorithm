e6fa060a6cf68318445b9f589f2f18cef478c5c9
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-02-03 11:08:10 +0900

    [Silver II] Title: 최대 힙, Time: 760 ms, Memory: 115192 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main{
+    public static String solution(int n, int[] command) {
+		StringBuffer answer = new StringBuffer();
+		
+		Queue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for (int i = 0; i < n; i++) {
+			int num = command[i];
+			
+			if (num == 0) {
+				
+				if (answer.length() != 0) {
+					answer.append("\n");
+				}
+				
+				if (q.isEmpty()) {
+					answer.append("0");
+				} else {
+					answer.append(q.poll());
+				}
+			} else {
+				q.add(num);
+			}
+		}
+		
+		return answer.toString();
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		int[] command = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			command[i] = sc.nextInt();
+		}
+		
+		System.out.println(solution(n, command));
+	}
+}
