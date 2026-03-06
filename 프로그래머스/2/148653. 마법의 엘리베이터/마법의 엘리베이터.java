8bae020772a5af0aeb4e32eb9d4bf300a74f8c7f
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-03-06 16:43:43 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while (storey != 0) {
+        	int temp = storey % 10;
+        	storey = storey / 10;
+        	
+        	if (temp == 5) {
+        		if (storey % 10 > 4) {
+        			answer += 10 - temp;
+        			storey++;
+        		} else {
+        			answer += temp;
+        		}
+        	} else if (temp > 5) {
+        		answer += 10 - temp;
+        		storey++;
+        	} else {
+        		answer += temp;
+        	}
+        	
+        }
+        
+        return answer;
+    }
+}
