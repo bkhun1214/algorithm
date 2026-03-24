9973f4fc6c42b457786b794985e73b137d9ef846
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-03-24 09:06:08 +0900

    [Gold IV] Title: 미로만들기, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static int solution(int n, int[][] arr) {
+		int answer = 0;
+		
+		int[][] costs = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(costs[i], Integer.MAX_VALUE);
+		}
+		
+		int[] dx = {-1, 1, 0, 0};
+		int[] dy = {0, 0, -1, 1};
+		
+		Deque<int[]> q = new LinkedList<>();
+		q.add(new int[] {0, 0, 0});
+		costs[0][0] = 0;
+		
+		while (!q.isEmpty()) {
+			int[] poll = q.pollFirst();
+			
+			if (poll[0] == n-1 && poll[1] == n-1) {
+				answer = poll[2];
+				break;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int x = poll[0] + dx[i];
+				int y = poll[1] + dy[i];
+				
+				if (x >= 0 && y >= 0 && x < n && y < n) {
+					
+					if (arr[x][y] == 1) {
+						
+						if (costs[x][y] > poll[2]) {
+							costs[x][y] = poll[2];
+							q.addFirst(new int[] {x, y, poll[2]});
+						}
+						
+					} else {
+						
+						if (costs[x][y] > poll[2]+1) {
+							costs[x][y] = poll[2]+1;
+							q.addLast(new int[] {x, y, poll[2]+1});
+						}
+					}
+					
+				}
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n][n];
+		
+		for (int i = 0; i < n; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = s.charAt(j)-'0';
+			}
+		}
+		
+		System.out.println(solution(n, arr));
+	}
+}
