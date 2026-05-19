63979f171caa773b343d2740aa59c2c8ac34ab08
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-05-19 15:48:40 +0900

    [level 2] Title: 큰 수 만들기, Time: 3885.07 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuffer answer = new StringBuffer();
+        int size = number.length() - k;
+        int index = 0;
+        
+        while (k > 0 && answer.length() < size) {
+            int max = 0;
+            int count = 0;
+            for (int i = 0; i < k+1; i++) {
+                int num = number.charAt(index+i) - '0';
+                if (max < num) {
+                    max = num;
+                    count = i;
+                }
+            }
+            
+            index += count+1;
+            answer.append(max);
+            k -= count;
+        }
+        
+        while (index < number.length() && answer.length() < size) {
+            answer.append(number.charAt(index));
+            index++;
+        }
+        
+        return answer.toString();
+    }
+}
