6b27e2910361bc55eab27a9f59baded4ab86f379
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-05-20 09:16:10 +0900

    [level 2] Title: 구명보트, Time: 6.01 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int lt = 0;
+        int rt = people.length - 1;
+        
+        while (lt <= rt) {
+            if (lt == rt) {
+                lt++;
+            } else if (people[rt] + people[lt] <= limit) {
+                rt--;
+                lt++;
+            } else {
+                rt--;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
