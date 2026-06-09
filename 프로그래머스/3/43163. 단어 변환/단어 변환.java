6aec61820dea012617abe6058ef80a980ca2ca12
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-06-09 14:45:01 +0900

    [level 3] Title: 단어 변환, Time: 0.10 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -44,10 +44,9 @@ class Solution {
     	for (int i = 0; i < word1.length(); i++) {
     		if (word1.charAt(i) != word2.charAt(i)) {
     			count++;
-    		}
-    		
-    		if (count > 1) {
+                if (count > 1) {
     			return false;
+    		    }
     		}
     	}
     	
