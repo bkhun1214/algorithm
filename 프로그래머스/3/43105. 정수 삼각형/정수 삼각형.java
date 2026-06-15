86f7bbbb762aa81d23eab7d5a10a4487a6b0788d
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-06-15 09:21:56 +0900

    [level 3] Title: 정수 삼각형, Time: 5.50 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -9,7 +9,6 @@ class Solution {
                 triangle[i][j] = Math.max(triangle[i][j] + triangle[i+1][j], triangle[i][j] + triangle[i+1][j+1]);
             }
         }
-        
         answer = triangle[0][0];
         
         return answer;
