5dbb435055777b2213e72568a3261af11ff0a099
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-08-13 09:31:46 +0900

    [level 1] Title: 문자열 나누기, Time: 0.56 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        int start = 0;
+        int compare = 0;
+        char temp = s.charAt(0);
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (temp == c) {
+                start += 1;
+            } else {
+                compare += 1;
+            }
+            
+            if (start == compare) {
+                answer ++;
+                start = 0;
+                compare = 0;
+                
+                if (i < s.length() - 1) {    
+                    temp = s.charAt(i+1);
+                }
+            }
+        }
+        
+        if (start != compare) {
+            answer++;
+        }
+        return answer;
+    }
+}
