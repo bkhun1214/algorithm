89a0f3ab6f98d70872ea71f310ee70e76c435e46
kihun <82800187+bkhun1214@users.noreply.github.com>
2026-08-20 13:02:34 +0900

    [level 2] Title: 점 찍기, Time: 5.98 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        
         for (int i = 0; i <= d; i += k) {
         	long temp = (long) Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2));
         	answer += (temp / k) + 1;
